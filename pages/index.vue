e38ca697060d0758857e7579786bee705b8ea0fe
Luis Fernando Rauber <rauberjuvinel@gmail.com>
2024-01-05 17:54:41 +0000

    Added a step section on homepage to indicate how user navigates

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,21 @@ useHead({
 })
 </script>
 <template>
-    <div class="flex flex-col items-center justify-center" style="height: 100vh">
-        <h1 class="text-center">
+    <div class="flex flex-col items-center justify-center gap-4" style="height: 100vh">
+        <h1 class="text-center mb-0">
             Welcome to Book of Terminology
         </h1>
-        <NuxtLink to="/terminologies" class="bg-green-800/20 p-4 rounded-lg text-xl font-semibold hover:bg-green-800/50 transition duration-150 ease-in-out">Discover More!</NuxtLink>
+        <ol class="list-decimal text-start text-xl list-inside">
+            <li>
+                Choose an area of your interest
+            </li>
+            <li>
+                Select a category
+            </li>
+            <li>
+                Enjoy terminologies
+            </li>
+        </ol>
+        <NuxtLink to="/terminologies" class="bg-green-800/20 p-4 rounded-lg text-xl font-semibold hover:bg-green-800/50 transition duration-150 ease-in-out">Start your Journey!</NuxtLink>
     </div>
 </template>
